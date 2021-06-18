882b3ec9b5b4023ef6cf3b9f6d2a725bb18ad05d
frankliang <7688325+frank-liang-lbm@users.noreply.github.comgit config --global user.email 7688325+frank-liang-lbm@users.noreply.github.com>
2021-06-18 11:01:24 +0800

    接口与回调（动作监视器）

---
+++ timer/TimerTest.java
@@ -0,0 +1,29 @@
+package timer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.Instant;
+
+public class TimerTest {
+    public static void main(String[] args) {
+        var listener=new TimePrinter();
+
+        var timer = new Timer(1000,listener);
+        timer.start();
+
+        JOptionPane.showMessageDialog(null,"Quit program?");
+        System.exit(0);
+
+    }
+}
+
+class TimePrinter implements ActionListener
+{
+    public void actionPerformed(ActionEvent event)
+    {
+        System.out.println("At the tone,the time is"+ Instant.ofEpochMilli(event.getWhen()));
+        Toolkit.getDefaultToolkit().beep();
+    }
+}
